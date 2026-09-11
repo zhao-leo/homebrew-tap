24ff523a72faf031814c8ef7775fcef68803f9f7
April <18055219130@163.com>
2026-09-11 14:48:42 +0800

    Remove postflight_steps from ext4fuse.rb
    Remove postflight_steps for notification display.

--- Formula/ext4fuse.rb
+++ Formula/ext4fuse.rb
@@ -23,10 +23,6 @@ class Ext4fuse < Formula
     bin.install "ext4fuse"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
   end
-
-  postflight_steps do
-    run "osascript", args: ["-e", "'display notification \"这是通知的具体内容\" with title \"通知标题\" subtitle \"这是副标题\"'"]
-  end
   
   test do
     # `test do` will create, run in and delete a temporary directory.
